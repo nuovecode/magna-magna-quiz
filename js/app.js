93ebb83d6251b887002d1e22493f997f224a9254
Irene <irene.iaccio@gmail.com>
2017-01-11 00:10:12 +0100

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,4 +1,33 @@
-var response = new Vue({
+var QuestionsMock =
+    [{
+      question: "a proposito di Capelli",
+      answer: [
+        { text: "naturali anche bianchi" , value: 2},
+        { text: "hennè con riccio naturale", value: 5}
+      ]
+    },
+    { question: "A suo tempo, nella vicenda relativa ai Marò, per chi tifavi",
+      answer: [
+        {text: "Marò", value: 0},
+        {text: "India", value: 1}
+      ]
+    }];
+
+var Quiz = new Vue({
+  el: '#answers',
+  data: {
+    value: 0,
+    items: QuestionsMock
+  },
+  methods: {
+    selectAnswer: function () {
+      this.value = 2;
+      console.log(QuestionsMock);
+    },
+  }
+})
+
+var result = new Vue({
   el: '#response',
   data: {
     response: 'il tuo profilo'
@@ -6,34 +35,6 @@ var response = new Vue({
   methods: {
     displayResponse: function () {
       this.response = 'RISPOSTA';
-    }
-  }
-})
-
-var Quiz = new Vue({
-  el: '#list',
-  data: {
-    value: 0,
-    items: [
-      {
-        question: "a proposito di Capelli",
-        response: [
-          { text: "naturali anche bianchi" , value: 2},
-          { text: "hennè con riccio naturale", value: 5}
-        ]
-      },
-      { question: "A suo tempo, nella vicenda relativa ai Marò, per chi tifavi",
-        response: [
-          {text: "Marò", value: 0},
-          {text: "India", value: 1}
-        ]
-      },
-    ]
-  },
-  methods: {
-    changeProject: function () {
-      this.value = 2
-      console.log(this.value);
     }
   }
 })
