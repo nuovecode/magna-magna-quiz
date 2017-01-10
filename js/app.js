d3b36c872b59ee6cf8100b6e6b773f89b3e5e3d4
Irene <irene.iaccio@gmail.com>
2017-01-10 22:40:09 +0100

    js directory

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var response = new Vue({
+  el: '#response',
+  data: {
+    response: 'il tuo profilo'
+  },
+  methods: {
+    displayResponse: function () {
+      this.response = 'RISPOSTA';
+    }
+  }
+})
+
+var Quiz = new Vue({
+  el: '#list',
+  data: {
+    value: 0,
+    items: [
+      {
+        question: "a proposito di Capelli",
+        response: [
+          { text: "naturali anche bianchi" , value: 2},
+          { text: "hennè con riccio naturale", value: 5}
+        ]
+      },
+      { question: "A suo tempo, nella vicenda relativa ai Marò, per chi tifavi",
+        response: [
+          {text: "Marò", value: 0},
+          {text: "India", value: 1}
+        ]
+      },
+    ]
+  },
+  methods: {
+    changeProject: function () {
+
+      this.value = 2
+      console.log(this.value);
+    }
+  }
+})
