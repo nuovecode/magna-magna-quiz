061da014acdc1293f309e49cf72544de61e3599d
Irene <irene.iaccio@gmail.com>
2017-01-12 23:33:54 +0100

    Added require and config file

--- js/app.js
+++ js/app.js
@@ -1,40 +1,50 @@
-var QuestionsMock =
-    [{
-      question: "a proposito di Capelli",
-      answer: [
-        { text: "naturali anche bianchi" , value: 2},
-        { text: "hennè con riccio naturale", value: 5}
-      ]
-    },
-    { question: "A suo tempo, nella vicenda relativa ai Marò, per chi tifavi",
-      answer: [
-        {text: "Marò", value: 0},
-        {text: "India", value: 1}
-      ]
-    }];
+requirejs(['app-config'], function(ar) {
 
-var Quiz = new Vue({
-  el: '#answers',
-  data: {
-    value: 0,
-    items: QuestionsMock
-  },
-  methods: {
-    selectAnswer: function () {
-      this.value = 2;
-      console.log(QuestionsMock);
-    },
-  }
-})
+  var QuestionsMock =
+      [{
+        question: "a proposito di Capelli",
+        answer: [
+          { text: "naturali anche bianchi" , value: 2},
+          { text: "hennè con riccio naturale", value: 5}
+        ]
+      },
+      { question: "A suo tempo, nella vicenda relativa ai Marò, per chi tifavi",
+        answer: [
+          {text: "Marò", value: 0},
+          {text: "India", value: 1}
+        ]
+      }];
 
-var result = new Vue({
-  el: '#response',
-  data: {
-    response: 'il tuo profilo'
-  },
-  methods: {
-    displayResponse: function () {
-      this.response = 'RISPOSTA';
+  var Quiz = new Vue({
+    el: '#answers',
+    data: {
+      value: 0,
+      items: QuestionsMock
+    },
+    methods: {
+      selectAnswer: function () {
+        this.value = 2;
+        QuestionsMock.forEach(function(el) {
+           for (var key in el) {
+                answer = el[key]
+                console.log(answer);
+                //console.log(ApiPath);
+           }
+        });
+      },
     }
-  }
-})
+  })
+
+  var result = new Vue({
+    el: '#response',
+    data: {
+      response: 'il tuo profilo'
+    },
+    methods: {
+      displayResponse: function () {
+        this.response = 'RISPOSTA';
+        console.log(ApiPaths);
+      }
+    }
+  })
+});
